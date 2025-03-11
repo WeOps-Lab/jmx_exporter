b0fcd98a2bc342e30bce738a8d0b3caebd5a1ee3
zhongming Fan <zhongmingfan@canway.net>
2025-03-11 13:59:47 +0800

    dp: update value

--- weops/pipe-tools/tomcat/charts/tomcat/helm_install.sh
+++ weops/pipe-tools/tomcat/charts/tomcat/helm_install.sh
@@ -5,7 +5,8 @@ object="tomcat"
 object_versions=("7.0.109" "8.5" "9.0" "10.1")
 
 # 设置起始端口号
-port=30880
+http_port=30880
+jmx_port=30990
 
 for version in "${object_versions[@]}"; do
     version_suffix="v$(echo "$version" | grep -Eo '[0-9]{1,2}\.[0-9]{1,2}' | tr '.' '-')"
@@ -13,8 +14,9 @@ for version in "${object_versions[@]}"; do
     helm install $object-$version_suffix --namespace $object -f ./values/bitnami_values.yaml ./$object \
     --set image.tag=$version \
     --set commonLabels.object_version=$version_suffix \
-    --set service.nodePorts.http=$port
-
-    ((port++))
+    --set service.nodePorts.http=$http_port \
+    --set service.extraPorts[0].nodePort=$jmx_port
+    ((http_port++))
+    ((jmx_port++))
     sleep 1
 done
